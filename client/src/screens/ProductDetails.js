d9fe72bcbaf4b9c340ac802552dae7bf1932a0a2
Naoufal El Youssoufi <n.elyousfi@gmail.com>
2020-03-25 18:49:46 +0100

    Add favorite products count to the navigation header and start working on the product details

--- client/src/screens/ProductDetails.js
+++ client/src/screens/ProductDetails.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import {useQuery} from '@apollo/client';
+import {GET_PRODUCT} from '../graphql/requests';
+import {Loading} from '../compoents/Loading';
+import {Error} from '../compoents/Error';
+import {Product} from '../compoents/Product';
 
-export function ProductDetails() {
-  return (
-    <View style={styles.container}>
-      <Text>This is the ProductDetails screen</Text>
-    </View>
-  );
+export function ProductDetails({route}) {
+  const {productId} = route.params;
+  const {loading, error, data} = useQuery(GET_PRODUCT, {
+    variables: {
+      productId,
+    },
+  });
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error error={error} />;
+  }
+
+  return <Product product={data.product} />;
 }
 
 const styles = StyleSheet.create({
